8c07b3d86c5767e4c42c672062ba7d88374882c9
MetizJigarJoshi <113347260+MetizJigarJoshi@users.noreply.github.com>
2025-09-03 10:49:55 +0530

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FarmerForm from './components/FarmerForm';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <FarmerForm />
+    </div>
+  );
+}
+
+export default App;
